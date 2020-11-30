a6a4b1f15d70ae0fbee24749138de418a67e83ee
Beau Weise <beauweise@gmail.com>
2020-11-30 10:39:33 -0600

    changed title

--- src/components/DetailsPage/DetailsPage.js
+++ src/components/DetailsPage/DetailsPage.js
@@ -19,18 +19,18 @@ class AboutPage extends Component {
         
           <Paper elevation={20} className='details'>
             {details.date &&
-            <p>Date:{this.props.store.details.date.slice(5, 7)
+            <p>Date: {this.props.store.details.date.slice(5, 7)
                 + '/' + this.props.store.details.date.slice(8, 10)
                 + '/' + this.props.store.details.date.slice(0, 4)}</p>}
-            <p>Lake:{details.lake}</p>
-            <p>Weather:{details.weather}</p>
-            <p>Wind:{details.wind}</p>
-            <p>Water Temp:{details.water_temp}°F</p>
-            <p>Water Clarity:{details.water_clarity}ft.</p>
-            <p>Total Fish Caught:{details.fish_count}</p>
-            <p>Total Fish Saw:{details.see_fish}</p>
-            <p>Best Lures of the Trip:{details.lures}</p>
-            <p>Notes for the Trip:{details.notes}</p>
+            <p>Lake: {details.lake}</p>
+            <p>Weather: {details.weather}</p>
+            <p>Wind: {details.wind}</p>
+            <p>Water Temp: {details.water_temp}°F</p>
+            <p>Water Clarity: {details.water_clarity}ft.</p>
+            <p>Total Fish Caught: {details.fish_count}</p>
+            <p>Total Fish Saw: {details.see_fish}</p>
+            <p>Best Lures of the Trip: {details.lures}</p>
+            <p>Notes for the Trip: {details.notes}</p>
             <br />
             <br />
             <Button variant="contained" onClick={this.backButton}>Back to History</Button>
